096f7daa3271c0db218ea2f17c0cb13077e6418a
Christopher Bartz <christopher.bartz@canonical.com>
2024-10-25 15:23:53 +0200

    WIP checkin

---
+++ webhook_redelivery/redelivery.py
@@ -0,0 +1,7 @@
+#  Copyright 2024 Canonical Ltd.
+#  See LICENSE file for licensing details.
+
+
+def redeliver_failed_webhooks(github_client: Github, last_state: datetime) -> None:
+    """Redeliver failed webhooks."""
+    # This is where the code to redeliver failed webhooks would go.
