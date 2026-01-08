756ee0ee9418fecd2c1a79c64f6516fe24f06867
yhaliaw <andrew.liaw@canonical.com>
2026-01-08 15:19:59 +0800

    Update license header to 2026 (#112)
    * Update license header to 2026
    
    * Update license check pattern

--- webhook_router/app.py
+++ webhook_router/app.py
@@ -1,4 +1,4 @@
-#  Copyright 2025 Canonical Ltd.
+#  Copyright 2026 Canonical Ltd.
 #  See LICENSE file for licensing details.
 
 """Flask application which receives GitHub webhooks and logs those."""
